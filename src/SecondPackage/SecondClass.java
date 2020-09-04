e094646cee0ba3e0331409e1738ea6bf216d3ada
kamal-gupta <kamal.gupta@clairvoyantsoft.com>
2020-09-04 23:39:50 +0530

    changes to the code

---
+++ src/SecondPackage/SecondClass.java
@@ -0,0 +1,11 @@
+package SecondPackage;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+
+        String name = "Welcome to the Git";
+        System.out.println(name);
+
+    }
+}
